7427085c559dd4e1f532bc4dfbf28fed7e77c89d
Ankit Srivastav <ankitshrivastav0008@gmail.com>
2026-03-31 20:28:56 +0530

    first commit

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,11 @@
+import {v2 as cloudinary} from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
+
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET
+});
+console.log(process.env.API_KEY,process.env.API_SECRET,process.env.CLOUD_NAME)
+export default cloudinary;
---
+++ frontend/utils/constant.js
@@ -0,0 +1,10 @@
+// export const USER_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/user";
+// export const JOB_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/job";
+// export const Application_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/application";
+// export const Company_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/company";
+
+
+export const USER_API_END_POINT="http://localhost:5000/api/v1/user";
+export const JOB_API_END_POINT="http://localhost:5000/api/v1/job";
+export const Application_API_END_POINT="http://localhost:5000/api/v1/application";
+export const Company_API_END_POINT="http://localhost:5000/api/v1/company";
